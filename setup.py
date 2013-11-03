5b24c3043c699dc3139658b5719c6915b10a730d
igniteflow <phil@potatolondon.com>
2013-11-03 21:34:36 +0000

    Typo request -> requests

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(
-	name='Codebase API Python Client',
-	version='1.0',
+	name='codebase-api-client',
+	version='1.1',
 	description='A Python client for the Codebase API',
 	author='Phil Tysoe',
 	author_email='philtysoe@gmail.com',
 	url='https://github.com/igniteflow/codebase-python-api-client',
-	packages=['request'],
+	packages=['requests'],
 )
