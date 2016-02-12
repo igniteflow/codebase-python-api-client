59748c05b18f2da53298ce746ba95dfc7de0d741
Alessandro Grazi <grazi.alessandro@gmail.com>
2016-02-12 13:53:31 +0000

    Remove commented code.

--- tests/decorators.py
+++ tests/decorators.py
@@ -8,56 +8,6 @@ import functools
 from mock import patch, mock_open
 
 
-#def patch_open(fn):
-#    @functools.wraps(fn)
-#    def wrapper(*args, **kwargs):
-#        file_fake = mock_open(read_data='{"cane": 2}')
-#        with patch.object(builtins, 'open', file_fake) as open_fake:
-#            args += (open_fake,)
-#            func_res = fn(*args, **kwargs)
-#            return func_res
-#
-#    return wrapper
-
-
-#def patch_open(read_data):
-#    def wrapper(fn):
-#        @functools.wraps(fn)
-#        def wrapped_func(*args, **kwargs):
-#            open_mocked = mock_open(read_data=read_data)
-#            with patch.object(builtins, 'open', open_mocked) as open_fake:
-#                args += (open_fake,)
-#                func_res = fn(*args, **kwargs)
-#                return func_res
-#        return wrapped_func
-#    return wrapper
-
-
-#class patch_open(object):
-#
-#    def __init__(self, read_data):
-#        self.read_data = read_data
-#        self.open_patch = patch.object(
-#            builtins,
-#            'open',
-#            mock_open(read_data=self.read_data),
-#        )
-#
-#    def __call__(self, fn):
-#        @functools.wraps(fn)
-#        def wrapped_func(*args, **kwargs):
-#            open_mock = self.__enter__()
-#            args += (open_mock,)
-#            func_res = fn(*args, **kwargs)
-#            self.__exit__()
-#            return func_res
-#        return wrapped_func
-#
-#    def __enter__(self):
-#        return self.open_patch.__enter__()
-#
-#    def __exit__(self, *args):
-#        self.open_patch.__exit__(*args)
 class patch_open(object):
 
     def __init__(self, read_data):
